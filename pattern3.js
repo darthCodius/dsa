c06466a5bd5e050d89bb85869603dd7520e33011
Abhinav Yadav <abhiyadavpky@gmail.com>
2025-07-19 12:02:12 +0530

    initial DSA Commit

--- pattern3.js
+++ pattern3.js
@@ -1,7 +1,7 @@
-function pattern3() {
-  for (let i = 0; i < 5; i++) {
+function pattern3(n) {
+  for (let i = 0; i < n; i++) {
     let row = "";
-    for (let j = 0; j < 5 - i; j++) {
+    for (let j = 0; j < n - i; j++) {
       row = row + (j + 1);
     }
     console.log(row);
